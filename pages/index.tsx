883973807929c0bc91c0d1d46df0f913f40c10d4
Mager <andrew.mager@gmail.com>
2024-07-04 10:03:43 -0500

    Homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,35 @@
 import React from "react";
 import type { GetServerSideProps } from "next";
+import Image from "next/image";
 import Layout from "../components/Layout";
-import Post, { PostProps } from "../components/Post";
-import prisma from "../lib/prisma";
+import { shuffle } from "../lib/util";
 import Title from "../components/Title";
 
+type GetFeaturedTracksResp = {
+  tracks: Track[];
+};
+
+type Track = {
+  image: string;
+  artist: string;
+  name: string;
+};
+
 export const getServerSideProps: GetServerSideProps = async () => {
-  const feed = await prisma.post.findMany({
-    where: {
-      published: true,
-    },
-    include: {
-      author: {
-        select: {
-          name: true,
-        },
-      },
-    },
-  });
+  const res = await fetch(
+    "https://occipital-cqaymsy2sa-uc.a.run.app/spotify/get_featured_tracks"
+  );
+  const resp: GetFeaturedTracksResp = await res.json();
   return {
-    props: { feed },
+    props: { tracks: shuffle(resp.tracks).slice(0, 12) },
   };
 };
 
 type Props = {
-  feed: PostProps[];
+  tracks: Track[];
 };
 
-const Blog: React.FC<Props> = (props) => {
+const Home: React.FC<Props> = ({ tracks }) => {
   return (
     <Layout>
       <div className="py-4">
@@ -37,9 +39,28 @@ const Blog: React.FC<Props> = (props) => {
             Share and discover your favorite tunes
           </span>
         </Title>
+        {tracks && (
+          <div className="mt-8 mb-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {tracks.map((t, index) => (
+              <div key={index} className="flex">
+                <div className="flex flex-col">
+                  <Image
+                    src={t.image}
+                    alt={`Track ${index}`}
+                    width={300}
+                    height={300}
+                    className="object-cover"
+                  />
+                  <div className="text-xl font-bold">{t.artist}</div>
+                  <div className="text-lg">{t.name}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </Layout>
   );
 };
 
-export default Blog;
+export default Home;
